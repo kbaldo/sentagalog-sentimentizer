51ea2a279bed5bf65adf0604efa962242d6a0630
Kevin Kyle Hendrick Baldo <kevinkbaldo@gmail.com>
2020-08-25 00:46:05 +0800

    Create temporary base theme

--- src/index.js
+++ src/index.js
@@ -4,11 +4,49 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { CssBaseline } from '@material-ui/core';
+
+ const theme = createMuiTheme({
+    palette: {
+        primary: {
+          light: '#5d8bc8',
+          main: '#265e97',
+          dark: '#003569',
+          contrastText: '#fff',
+        },
+        secondary: {
+          light: '#d05d73',
+          main: '#9b2c48',
+          dark: '#680021',
+          contrastText: '#fff',
+        },
+        warning: {
+            light: '#ffbd5c',
+            main: '#ed8c2c',
+            dark: '#b55e00',
+            contrastText: '#fff',
+          },
+      },
+    overrides: {
+       
+      // Style sheet name ⚛️
+      MuiButton: {
+        // Name of the rule
+        text: {
+          // Some CSS
+
+        },
+      },
+    },
+  });
+
+  ReactDOM.render(
+    <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />,
+    </ThemeProvider>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
