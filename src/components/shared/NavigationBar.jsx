08b5f1d95c4cf622ee1580576627c72194340b5a
Kevin Kyle Hendrick Baldo <kevinkbaldo@gmail.com>
2020-08-25 01:26:54 +0800

    Update NavigationBar.jsx

--- src/components/shared/NavigationBar.jsx
+++ src/components/shared/NavigationBar.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
+//import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
+//import IconButton from "@material-ui/core/IconButton";
+//import MenuIcon from "@material-ui/icons/Menu";
 import { Grid } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
