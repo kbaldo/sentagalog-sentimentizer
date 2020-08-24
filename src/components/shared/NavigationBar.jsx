69e96b37332f90146e43712a98535119a768c6e9
Kevin Kyle Hendrick Baldo <kevinkbaldo@gmail.com>
2020-08-25 01:24:06 +0800

    Update NavigationBar.jsx

--- src/components/shared/NavigationBar.jsx
+++ src/components/shared/NavigationBar.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import { Grid } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -12,9 +14,6 @@ const useStyles = makeStyles((theme) => ({
   },
   menuButton: {
     marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
   },
 }));
 
@@ -25,10 +24,27 @@ export default function NavigationBar() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Button color="inherit">Login</Button>
+          <Grid
+            justify="space-between" // Add it here :)
+            container
+            alignItems="center"
+          >
+            <Grid item>
+              <IconButton
+                edge="start"
+                className={classes.menuButton}
+                color="inherit"
+                aria-label="menu"
+              >
+                <MenuIcon />
+              </IconButton>
+            </Grid>
+            <Grid item>
+              <Button color="inherit">About the project</Button>
+              <Button color="inherit">Proponents</Button>
+              <Button color="inherit">Download Paper</Button>
+            </Grid>
+          </Grid>
         </Toolbar>
       </AppBar>
     </div>
