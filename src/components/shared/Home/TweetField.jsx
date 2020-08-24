6f9d4f8abc56ba600fc7de535ecadf5084889df4
Kevin Kyle Hendrick Baldo <kevinkbaldo@gmail.com>
2020-08-25 01:58:20 +0800

    Create TweetField.jsx

---
+++ src/components/shared/Home/TweetField.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import { Button } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
+
+export default function MultilineTextFields() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState('Controlled');
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <form className={classes.root} noValidate autoComplete="off">
+        <TextField
+          id="tweet"
+          label="Tweet"
+          multiline
+          rows={4}
+          defaultValue="Paste tweet text here"
+          variant="outlined"
+        />
+        <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+          >
+            Sign In
+          </Button>
+    </form>
+  );
+}
