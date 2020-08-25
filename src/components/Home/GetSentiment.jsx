b11682f49d4e2605be77dc24fd1129952c5865a7
Kevin Kyle Hendrick Baldo <kevinkbaldo@gmail.com>
2020-08-25 23:17:24 +0800

    Update placeholder responseType text

--- src/components/Home/GetSentiment.jsx
+++ src/components/Home/GetSentiment.jsx
@@ -89,7 +89,7 @@ export default function GetSentiment(props) {
         aria-labelledby="responsive-dialog-title"
       >
         <DialogTitle id="responsive-dialog-title">
-          {"The text uses " + responseType + " language!"}
+          {"The text uses _" + responseType + "_ language!"}
         </DialogTitle>
         <DialogContent>
 		  <Paper variant="outlined" square><div className={classes.contents}>{props.text}</div></Paper>
