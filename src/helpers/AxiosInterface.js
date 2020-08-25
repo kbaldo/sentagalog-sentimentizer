418344b9afdaf1aa4d710101ea0b767b29b2737b
Kevin Kyle Hendrick Baldo <kevinkbaldo@gmail.com>
2020-08-25 22:16:58 +0800

    Create AxiosInterface.js

---
+++ src/helpers/AxiosInterface.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+class AxiosInterface
+{
+	SendAsPost(payload, callbacks) //Do-it-all
+	{
+		const serverAddress = `https://jsonplaceholder.typicode.com/posts`;
+
+		callbacks.request(payload); //Executed in importing component.
+		axios.post(serverAddress, payload)
+			.then(
+				response => {
+					callbacks.success(response.data); //Executed in importing component.
+				}
+			)
+			.catch(
+				error => {
+					callbacks.failure(error); //Executed in importing component.
+				}
+			)
+	}
+}
+
+export default new AxiosInterface();
