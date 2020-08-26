a26632d9259bc3f8a7ebbde3a15753fbcd16754d
Kevin Kyle Hendrick Baldo <kevinkbaldo@gmail.com>
2020-08-26 23:32:28 +0800

    Moved theme source code to App.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,48 @@ import Home from './pages/Home';
 import About from './pages/About';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { CssBaseline } from '@material-ui/core';
+
+ const theme = createMuiTheme({
+    palette: {
+        primary: {
+          light: '#8e82ff',
+          main: '#5755d9',
+          dark: '#082ca6',
+          contrastText: '#fff',
+        },
+        secondary: {
+          light: '#f15d75',
+          main: '#ba274a',
+          dark: '#840023',
+          contrastText: '#fff',
+        },
+        warning: {
+            light: '#ffbd5c',
+            main: '#ed8c2c',
+            dark: '#b55e00',
+            contrastText: '#fff',
+          },
+      },
+    overrides: {
+       
+      // Style sheet name ⚛️
+      MuiButton: {
+        // Name of the rule
+        text: {
+          // Some CSS
+
+        },
+      },
+    },
+  });
+
 function App() {
   return (
     <div className="App">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
       <Router path="/">
         <NavigationBar />
       <Switch>
@@ -15,6 +54,7 @@ function App() {
         <Route exact path={"/about"} render={(routerProps) => (<About routerProps={routerProps} />)}/>
       </Switch>
       </Router>
+      </ThemeProvider>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,48 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
-import { CssBaseline } from '@material-ui/core';
-
- const theme = createMuiTheme({
-    palette: {
-        primary: {
-          light: '#8e82ff',
-          main: '#5755d9',
-          dark: '#082ca6',
-          contrastText: '#fff',
-        },
-        secondary: {
-          light: '#f15d75',
-          main: '#ba274a',
-          dark: '#840023',
-          contrastText: '#fff',
-        },
-        warning: {
-            light: '#ffbd5c',
-            main: '#ed8c2c',
-            dark: '#b55e00',
-            contrastText: '#fff',
-          },
-      },
-    overrides: {
-       
-      // Style sheet name ⚛️
-      MuiButton: {
-        // Name of the rule
-        text: {
-          // Some CSS
-
-        },
-      },
-    },
-  });
-
   ReactDOM.render(
-    <ThemeProvider theme={theme}>
-        <CssBaseline />
         <App />,
-    </ThemeProvider>,
     document.getElementById('root')
 );
 
